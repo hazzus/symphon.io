f031fda373c2772f8d11458f4f3bdebe12c44351
Pavel Bezborodov <36053554+hazzus@users.noreply.github.com>
2018-11-10 01:44:04 +0300

    Delete forms.py

--- symphonio/compface/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class PhotoForm(forms.Form):
-    photo = forms.ImageField(
-        widget=forms.FileInput(
-            attrs={'class': 'form-control', 'style': 'display: none;'}
-        )
-    )
