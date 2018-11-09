c65b633b90df496df6f0c84b9985e3fa85eec880
Konstantin Nisht <zeron.n1@yandex.ru>
2018-11-09 17:52:14 +0300

    changed test file

--- symphonio/compface/tests.py
+++ symphonio/compface/tests.py
@@ -1,9 +1,1 @@
 from django.test import TestCase
-import symphonio.compface.recognize as recognize
-
-
-class FaceRecognitionTestCase(TestCase):
-
-    def test_DiCaprio_is_recognized(self):
-        result = recognize.recognize("dicaprio2")
-        self.assertEqual(result, True)
