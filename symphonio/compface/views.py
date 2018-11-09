dc9a146e6fcfcf2a93d05db51eaee47855e7607d
hazzus <dfa.sro591@gmail.com>
2018-11-09 18:26:09 +0300

    Index view and forms

--- symphonio/compface/views.py
+++ symphonio/compface/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .forms import PhotoForm
 # Create your views here.
+
+def index(request):
+    if request.method == 'GET':
+        photo_form = PhotoForm()
+        return render(request, "index.html", {'form': photo_form})
+    else:
+        pass
