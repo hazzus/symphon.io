9b42b582b87d271c19e19e403a7bf883ce90eb0b
Konstantin Nisht <zeron.n1@yandex.ru>
2018-11-10 00:47:56 +0300

    added db integration

--- symphonio/compface/trainer.py
+++ symphonio/compface/trainer.py
@@ -1,27 +1,22 @@
 import face_recognition
+from compface.models import ComposerRecognitionData, Composer
+from compface.recognize import known_faces, ids
 import pickle
-from symphonio.compface.models import ComposerRecognitionData
-
-
-def train(composer_connections):
-    train_data = []
-    for (filename, id) in composer_connections:
-        loaded_image = face_recognition.load_image_file(filename)
-        encoding = face_recognition.face_encodings(loaded_image)
-        try:
-            train_data.append((encoding[0], id))
-        except IndexError:
-            print("Could not get face from " + filename + " of " + str(id))
-    file = open("train_data", "wb")
-    pickle.dump(train_data, file)
-    file.close()
+import numpy
 
 
 def add_composer_encoding(id, image):
     """
     Raises FaceNotFound exception
     """
-    encoding = face_recognition.face_encodings(image)
-    composer = ComposerRecognitionData.objects.create(composer=id, data=encoding)
-    ComposerRecognitionData.objects.add()
-
+    image = numpy.array(image)
+    try:
+        encoding = face_recognition.face_encodings(image)[0]
+    except IndexError:
+        print("Could not find any face")
+        return
+    composer = Composer.objects.get(pk=id)
+    composer_encoded = ComposerRecognitionData.objects.create(composer=composer, data=pickle.dumps(encoding))
+    composer_encoded.save()
+    known_faces.append(encoding)
+    ids.append(composer.id)
