aa5054608c7a397d56d881c88f928c1d29848d90
hazzus <dfa.sro591@gmail.com>
2018-11-09 18:42:22 +0300

    Templates fix

--- symphonio/compface/views.py
+++ symphonio/compface/views.py
@@ -3,8 +3,6 @@ from .forms import PhotoForm
 # Create your views here.
 
 def index(request):
-    if request.method == 'GET':
-        photo_form = PhotoForm()
-        return render(request, "index.html", {'form': photo_form})
-    else:
-        pass
+    photo_form = PhotoForm()
+    return render(request, 'index.html', {'form': photo_form})
+
