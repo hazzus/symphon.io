d62cfbce4033dbfe30be3829e627dfc0cae2cc31
Alexey <gusevalexey0@gmail.com>
2018-11-10 04:24:24 +0300

    small fixes

--- symphonio/compface/urls.py
+++ symphonio/compface/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('recognize', views.recognize),
-    path('composers/<int:composer_id>/', views.composers),
+    path('composers/<int:composer_id>/', views.composer),
     path('composers/<int:composer_id>/affiche', views.affiche)
 ]
