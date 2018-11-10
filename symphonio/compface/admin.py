4f4f9984e24b93da6f23b3643c7ee57dba24aabd
Konstantin Nisht <zeron.n1@yandex.ru>
2018-11-11 02:33:50 +0300

    expanded names

--- symphonio/compface/admin.py
+++ symphonio/compface/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
-from .models import Composer, Composition, Concert, ComposerRecognitionData
+from .models import Composer, Composition, Concert
 
 admin.site.register(Composer)
 admin.site.register(Composition)
