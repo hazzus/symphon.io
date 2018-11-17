5260f6cb6a560ea909c2c7ded58cea0bd4ee3be7
Konstantin Nisht <zeron.n1@yandex.ru>
2018-11-17 19:04:02 +0300

    fix slicing composer bug

--- symphonio/compface/tests.py
+++ symphonio/compface/tests.py
@@ -10,17 +10,17 @@ images_dir = os.path.join('compface', 'img')
 
 
 class BasicRecognitionTestCase(TestCase):
-    tchaikovsy = os.path.join(images_dir, 'Tchaikovsky.jpg')
+    tchaikovsky = os.path.join(images_dir, 'Tchaikovsky.jpg')
     another_tchaikovsky = os.path.join(images_dir, 'Tchaikovsky_2.jpg')
     rakhmaninov = os.path.join(images_dir, 'Rakhmaninov.jpg')
     forest = os.path.join(images_dir, 'forest.jpg')
 
     def setUp(self):
         self.composer = Composer.objects.create(name="Test1", bio="test",
-                                                photo=ImageFile(open(self.tchaikovsy, "rb")))
+                                                photo=ImageFile(open(self.tchaikovsky, "rb")))
 
     def test_the_same_recognize(self):
-        image = Image.open(open(self.tchaikovsy, "rb"))
+        image = Image.open(open(self.tchaikovsky, "rb"))
         result = recognize_image(image)
         self.assertEqual(result, [self.composer.id])
 
@@ -54,20 +54,12 @@ def count_matches(images_dir, composer):
     return count, success
 
 
-class StatisticTestCase(TestCase):
+class StatisticOnePhotoTestCase(TestCase):
 
     def setUp(self):
         # Learning one photo
         Composer.objects.create(first_name="Petr", name="Tchaikovsky One", bio="norm",
                                 photo=ImageFile(open(os.path.join(images_dir, "Tchaikovsky.jpg"), "rb")))
-
-        # Learning all photos
-        t_dir = os.path.join(images_dir, 'tchaik')
-        t_listdir = os.listdir(t_dir)
-        c = Composer.objects.create(first_name="Petr", name="Tchaikovsky All", bio="norm",
-                                    photo=ImageFile(open(os.path.join(t_dir, t_listdir[0]), "rb")))
-        for image in t_listdir[1:]:
-            add_composer_encoding(c.id, Image.open(open(os.path.join(t_dir, image), 'rb')))
 
     def test_tchaik_clear_one_stat(self):
         composer = Composer.objects.get(name="Tchaikovsky One")
@@ -77,6 +69,18 @@ class StatisticTestCase(TestCase):
 
         print('Efficiency:', success / count)
         self.assertEqual(success, count)
+
+
+class StatisticMultiplePhotoTestCase(TestCase):
+
+    def setUp(self):
+        # Learning all photos
+        t_dir = os.path.join(images_dir, 'tchaik')
+        t_listdir = os.listdir(t_dir)
+        c = Composer.objects.create(first_name="Petr", name="Tchaikovsky All", bio="norm",
+                                    photo=ImageFile(open(os.path.join(t_dir, t_listdir[0]), "rb")))
+        for image in t_listdir[1:]:
+            add_composer_encoding(c.id, Image.open(open(os.path.join(t_dir, image), 'rb')))
 
     def test_tchaik_clear_many_stat(self):
         composer = Composer.objects.get(name="Tchaikovsky All")
